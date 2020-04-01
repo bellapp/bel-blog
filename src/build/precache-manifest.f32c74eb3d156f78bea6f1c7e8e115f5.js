4c42efe37fc760a039931421dbcef945a5f3eb29
babel2015 <bellout@gmail.com>
2020-04-01 19:10:37 +0100

    First Commit

---
+++ src/build/precache-manifest.f32c74eb3d156f78bea6f1c7e8e115f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dee31cc6949c7b5d5e651ad606b5e2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "751443a1ea27b93fa4f8",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "5223d3b226526ae3f646",
+    "url": "/static/js/2.b3196c28.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.b3196c28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "751443a1ea27b93fa4f8",
+    "url": "/static/js/main.a2de6824.chunk.js"
+  },
+  {
+    "revision": "b417801579a5ebffb3df",
+    "url": "/static/js/runtime-main.6e0da9ba.js"
+  }
+]);
